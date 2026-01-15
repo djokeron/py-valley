05af11339e328d1eb82b408fbc717aec7e5071b4
HunterSlime192 <kadushkin18@bk.ru>
2026-01-15 17:11:55 +0400

    changed pictures

--- Images/hous.tsx
+++ Images/hous.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="hous" tilewidth="64" tileheight="64" tilecount="16" columns="4">
- <image source="houstile.png" width="256" height="256"/>
+<tileset version="1.10" tiledversion="1.11.2" name="hous" tilewidth="128" tileheight="128" tilecount="16" columns="4">
+ <image source="houstile.png" width="512" height="512"/>
 </tileset>
