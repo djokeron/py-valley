8a0d551778f99080dbb07bb272a45ebb8fe3afb6
HunterSlime192 <kadushkin18@bk.ru>
2026-01-24 00:31:42 +0400

    Compiled exe file

---
+++ The_Game/py-valley/Data/Map/Shop3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Shop3" tilewidth="64" tileheight="64" tilecount="64" columns="4">
+ <image source="../Images/Shop3.png" width="256" height="256"/>
+</tileset>
